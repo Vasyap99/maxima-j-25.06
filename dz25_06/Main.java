79a8acce661f68685181014647a30bc85a571027
kko <kkl24@mail.ru>
2024-06-29 22:37:57 +0300

    ok4+dop

--- dz25_06/Main.java
+++ dz25_06/Main.java
@@ -5,24 +5,31 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[]s1){
         TVset ts=new TVset(new Channel1("TNT",new Program("Dom 2"),new Program("Bitva Ekstrasensov")),
-                           new Channel1("1 kanal",new Program("Novosti"),new Program("KVN"),new Program("Show Malahova"))
+                           new Channel1("1 kanal",new Program("Novosti"),new Program("KVN"),new Program("Show Malahova")),
+                           new Channel1("Russia24",new Program("Vesti"))
                           );
         Pult1 pult=new Pult1();
         ts.registerPult(pult);
 
         Scanner s=new Scanner(System.in);
 
-        while(s.hasNextInt()){
-            int i=s.nextInt();
-            switch(i){
-                case 0:
-                    pult.switchChannelForward();
-                    break;
-                case -1:
-                    pult.switchChannelBack();
-                    break;
-                default:
-                    pult.switchChannelTo(i);
+        while(s.hasNext()){
+            if(s.hasNextInt()){
+                int i=s.nextInt();
+                switch(i){
+                    case 0:
+                        pult.switchChannelForward();
+                        break;
+                    case -1:
+                        pult.switchChannelBack();
+                        break;
+                    default:
+                        pult.switchChannelTo(i);
+                }
+            }else{
+                String cmd=s.next();
+                if(cmd.equals("h")) pult.switchToPreviousChannel();
+                else break;
             }
         }
     }
--- dz25_06/Pult1.java
+++ dz25_06/Pult1.java
@@ -6,7 +6,9 @@ import java.util.LinkedList;
 public class Pult1 extends AbstractPult implements Pult{
     //* Метод для переключения по цифрам (Т.е. ввел цифру 5 - включился 5 канал, цифру 8 - включился 8 и т.д.)
 
-    LinkedList<Integer> l=new LinkedList<>();
+    private LinkedList<Integer> l=new LinkedList<>();
+    private boolean b=false;
+    private int x=0;
 
     {
        l.add(1);
@@ -18,6 +20,17 @@ public class Pult1 extends AbstractPult implements Pult{
 
     public void switchChannelTo(int i){
         getTs().switchChannel(this,i);
-        memChannel(getTs().getChNum());
+        memChannel(getTs().getCurrChannel());
+        b=false;
+    }
+
+    //* Доп. метод (доп задача) чтобы можно было переключаться между последними каналами (Т.е. если вы были на 5 канале, а до этого были на 8, то при вызове этого метода, он должен переключить нас на 8 канал, если вызвать его еще раз, то на 5 канал)
+    public void switchToPreviousChannel(){
+        int sz=l.size();
+        if(b) sz=--x; else x=sz;
+        if(sz>1){
+            switchChannelTo(l.get(sz-2));
+        }
+        b=true;
     }
 }
