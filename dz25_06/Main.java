952b35a6a3b7757c70700f341d6c6a8fe08f6479
kko <kkl24@mail.ru>
2024-06-29 19:44:35 +0300

    ok0

--- dz25_06/Main.java
+++ dz25_06/Main.java
@@ -1,14 +1,29 @@
 package dz25_06;
 
+import java.util.Scanner;
+
 public class Main{
-    public static void main(String[]s){
+    public static void main(String[]s1){
         TVset ts=new TVset(new Channel1("TNT",new Program("Dom 2"),new Program("Bitva Ekstrasensov")),
                            new Channel1("1 kanal",new Program("Novosti"),new Program("KVN"),new Program("Show Malahova"))
                           );
         Pult1 pult=new Pult1();
         ts.registerPult(pult);
-        pult.switchChannelTo(1);
-        pult.switchChannelTo(2);
-        System.out.println("Test");
+
+        Scanner s=new Scanner(System.in);
+
+        while(s.hasNextInt()){
+            int i=s.nextInt();
+            switch(i){
+                case 0:
+                    pult.switchChannelForward();
+                    break;
+                case -1:
+                    pult.switchChannelBack();
+                    break;
+                default:
+                    pult.switchChannelTo(i);
+            }
+        }
     }
 }
