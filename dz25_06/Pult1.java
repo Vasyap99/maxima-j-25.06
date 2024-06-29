117a3a8abeab5278fab6fc6cff51054d645bcc45
kko <kkl24@mail.ru>
2024-06-29 21:10:28 +0300

    ok1

--- dz25_06/Pult1.java
+++ dz25_06/Pult1.java
@@ -1,6 +1,6 @@
 package dz25_06;
 
-public class Pult1 extends AbstractPult{
+public class Pult1 extends AbstractPult implements Pult{
     //* Метод для переключения по цифрам (Т.е. ввел цифру 5 - включился 5 канал, цифру 8 - включился 8 и т.д.)
     public void switchChannelTo(int i){
         getTs().switchChannel(this,i);
