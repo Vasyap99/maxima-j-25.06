6c04c65e1458ee4b334474656943fff847729d24
kko <kkl24@mail.ru>
2024-08-04 16:24:35 +0300

    2

--- src/main/java/dz25_06/Program.java
+++ src/main/java/dz25_06/Program.java
@@ -1,14 +1,13 @@
 package dz25_06;
 
 import lombok.AllArgsConstructor;
-
+import lombok.ToString;
+import lombok.Getter;
 
 @AllArgsConstructor
+@ToString(includeFieldNames = false)
+@Getter
 public class Program{
     private final String name;
-
-    public String toString(){
-        return name;
-    }
 }
 
