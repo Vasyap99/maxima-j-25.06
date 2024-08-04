fa7156573acc45c0d8b465d55f8f276166056c50
kko <kkl24@mail.ru>
2024-08-04 18:00:42 +0300

    4-fix

--- src/main/java/dz25_06/Channel1.java
+++ src/main/java/dz25_06/Channel1.java
@@ -7,11 +7,14 @@ import lombok.ToString;
 
 @ToString(includeFieldNames = false)
 public class Channel1{
+    @ToString.Exclude
     private final List<Program> programs;
+
     public Channel1(String name,Program ... ps){
         programs=Arrays.asList(ps);
         this.name=name;
     }
+
     public String getCurrProgram(){
         try{
             return programs.get(0).toString();
