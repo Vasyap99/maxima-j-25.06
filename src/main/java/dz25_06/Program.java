18e0eace18350a1504813f663524a3643e1c4487
kko <kkl24@mail.ru>
2024-08-11 19:16:04 +0300

    9 @Value variant

--- src/main/java/dz25_06/Program.java
+++ src/main/java/dz25_06/Program.java
@@ -1,15 +1,9 @@
 package dz25_06;
 
-import lombok.AllArgsConstructor;
-import lombok.ToString;
-import lombok.Getter;
-import lombok.EqualsAndHashCode;
+import lombok.Value;
 
-@AllArgsConstructor
-@ToString(includeFieldNames = false)
-@Getter
-@EqualsAndHashCode
+@Value
 public class Program{
-    private final String name;
+    String name;
 }
 
