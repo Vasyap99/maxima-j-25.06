82e2205fb8eca6635cf367386b2fd75eeb91036c
kko <kkl24@mail.ru>
2024-08-11 18:34:48 +0300

    8+EqualsAndHashCode

--- src/main/java/dz25_06/Program.java
+++ src/main/java/dz25_06/Program.java
@@ -3,10 +3,12 @@ package dz25_06;
 import lombok.AllArgsConstructor;
 import lombok.ToString;
 import lombok.Getter;
+import lombok.EqualsAndHashCode;
 
 @AllArgsConstructor
 @ToString(includeFieldNames = false)
 @Getter
+@EqualsAndHashCode
 public class Program{
     private final String name;
 }
